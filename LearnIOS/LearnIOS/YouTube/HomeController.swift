277bb4f288b6979e8dba508dbe5ea808ba7866ad
Nerneen Mohamed <nermeen@enhance.online>
2019-03-18 20:06:38 +0200

    clean proj

--- LearnIOS/LearnIOS/YouTube/HomeController.swift
+++ LearnIOS/LearnIOS/YouTube/HomeController.swift
@@ -14,22 +14,17 @@ class HomeController: UICollectionViewController ,UICollectionViewDelegateFlowLa
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        if L102Language.currentAppleLanguage() == "ar" {
-//            loopThroughSubViewAndFlipTheImageIfItsAUIImageView(subviews: self.view.subviews)
-//        }
-//        
+ 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
-        setupNavigation()
-
-        // Register cell classes
-        self.collectionView!.register(VideoCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-        
+       
         // Do any additional setup after loading the view.
+        setupViews()
+
+
     }
-    func setupNavigation(){
+    func setupViews(){
+        //collection view
         let flowLayout = UICollectionViewFlowLayout()
         collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: flowLayout)
         collectionView.alwaysBounceVertical = true
@@ -37,74 +32,29 @@ class HomeController: UICollectionViewController ,UICollectionViewDelegateFlowLa
         collectionView.backgroundColor = .white
         collectionView.contentInset = UIEdgeInsets(top: 50, left: 0, bottom: 0, right: 0)
         collectionView.scrollIndicatorInsets = UIEdgeInsets(top: 50, left: 0, bottom: 0, right: 0)
-        navigationController?.navigationBar.isTranslucent = false
+        // Register cell classes
+        self.collectionView!.register(VideoCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         
+        //navigation bar
+        navigationController?.navigationBar.isTranslucent = false
         let titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width - 32, height: view.frame.height))
         titleLabel.text = "Home"
         titleLabel.font = UIFont.systemFont(ofSize: 20)
         titleLabel.textColor = .white
         navigationItem.titleView = titleLabel
         
-        let titleButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.width - 32, height: view.frame.height))
-        titleButton.setTitle("Home", for: .normal)
-       titleButton.setTitleColor(.white, for: .normal)
-        navigationItem.titleView = titleButton
-        titleButton.addTarget(self, action: #selector(switchLanguage), for: .touchUpInside)
-        
+        //setup menu bar
+        view.addSubview(menuBar)
+        view.addConstraintsWithFormat("H:|[v0]|", views: menuBar)
+        view.addConstraintsWithFormat("V:|[v0(50)]", views: menuBar)
      }
-    @objc func switchLanguage(){
-//
-//        var transition: UIView.AnimationOptions = .transitionFlipFromLeft
-//        if L102Language.currentAppleLanguage() == "en" {
-//            L102Language.setAppleLAnguageTo(lang: "ar")
-//            UIView.appearance().semanticContentAttribute = .forceRightToLeft
-//        } else {
-//            L102Language.setAppleLAnguageTo(lang: "en")
-//            transition = .transitionFlipFromRight
-//            UIView.appearance().semanticContentAttribute = .forceLeftToRight
-//        }
-//        let rootviewcontroller: UIWindow = ((UIApplication.shared.delegate?.window)!)!
-//        let flowLayout = UICollectionViewFlowLayout()
-//        let vc = HomeController(collectionViewLayout: flowLayout)
-//        let navController = UINavigationController(rootViewController: vc)
-//        rootviewcontroller.rootViewController = navController
-//        
-//        
-        
-//        rootviewcontroller.rootViewController = self.storyboard?.instantiateViewController(withIdentifier: "rootnav")
-
-//        let mainwindow = (UIApplication.shared.delegate?.window!)!
-//        mainwindow.backgroundColor = UIColor(hue: 0.6477, saturation: 0.6314, brightness: 0.6077, alpha: 0.8)
-//        UIView.transition(with: mainwindow, duration: 0.55001, options: transition, animations: { () -> Void in
-//        }) { (finished) -> Void in
-//            
-//        }
-     }
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        setupMenuBar()
-    }
     let menuBar : MenuBar = {
         let menu = MenuBar()
         return menu
     }()
-   private func setupMenuBar(){
-        view.addSubview(menuBar)
+  
     
-    view.addConstraintsWithFormat("H:|[v0]|", views: menuBar)
-    view.addConstraintsWithFormat("V:|[v0(50)]", views: menuBar)
-    
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
 
     // MARK: UICollectionViewDataSource
 
